de15d7760f716d46750f7fb4fee6eb57219659b8
Danilopc <sr_danilo@hotmail.com>
2023-02-06 17:16:41 +0100

    add average temp

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_KEY = `3b2cfc2c634fd33d5f046d2a97118c96`;
 const temperature = document.getElementById('temperature');
 const cityPlacement = document.getElementById('city');
 const country = document.getElementById('country');
+const average = document.getElementById('average');
 
 function setCity (){
 const city = document.getElementById('city_name').value
@@ -19,13 +20,22 @@ function fetchCityName(e){
     let longitude = data[0].lon;
     let countryName = data[0].country;
     let cityName = data[0].name;
+    
     const fetchWeather = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
   .then((response)=> response.json())
  .then(data => {console.log(data)
 
-const curremtTemp = data.hourly.temperature_2m[0]
+const curremtTemp = data.hourly.temperature_2m[0];
+const arr = data.hourly.temperature_2m; 
+let sum = 0;
+for (let number of arr) {
+    sum += number;
+}
 
+averageTemp = sum /arr.length;
+console.log(averageTemp)
 
+average.innerHTML="AVERAGE TEMPERATURE: " + parseInt(averageTemp)
 temperature.innerHTML = curremtTemp
 cityPlacement.innerHTML= cityName
 country.innerHTML = countryName
@@ -36,16 +46,5 @@ country.innerHTML = countryName
 .catch((error)=> console.log(error))
 }
 
-
-
-// }
-    // const city = data.name;
-    // 
-    // const description = data.weather[0].description;
-  
-    // document.getElementById("city").innerHTML = city;
-    // document.getElementById("temperature").innerHTML = temperature;
-    // document.getElementById("weather-description").innerHTML = description;;
-  
 
 
