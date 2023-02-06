0c277336409e16cf9ec6c5300483ccc86ffdb6c3
Danilopc <sr_danilo@hotmail.com>
2023-02-06 17:55:35 +0100

    test3 https

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ fetchCityName(city)
 }
 function fetchCityName(e){  
 
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${e}&limit=5&appid=${API_KEY}`)
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${e}&limit=5&appid=${API_KEY}`)
 .then((response) => response.json())
   .then((data) => {
     console.log(data);
