109d5f36d5d19a114d4392c4ab34748b18dd8c99
Danilopc <sr_danilo@hotmail.com>
2023-02-06 22:11:07 +0100

    update tailwind

--- script.js
+++ script.js
@@ -3,50 +3,50 @@ const temperature = document.getElementById('temperature');
 const cityPlacement = document.getElementById('city');
 const country = document.getElementById('country');
 const average = document.getElementById('average');
+const city = document.getElementById('city-name')
+city.addEventListener("keyup", function(e){
+ if(e.code === 'Enter'){
+    setCity();
+ }
+})
+function setCity() {
 
-function setCity (){
-const city = document.getElementById('city_name').value
-
-fetchCityName(city)
+  fetchCityName(city.value)
 
 }
-function fetchCityName(e){  
+function fetchCityName(e) {
 
   fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${e}&limit=5&appid=${API_KEY}`)
-.then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    let latitude = data[0].lat;
-    let longitude = data[0].lon;
-    let countryName = data[0].country;
-    let cityName = data[0].name;
-    
-  //   const otherInfo = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m_max`)
-  // .then((response)=> response.json())
-  // .then((data) => console.log(data))
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let latitude = data[0].lat;
+      let longitude = data[0].lon;
+      let countryName = data[0].country;
+      let cityName = data[0].name;
 
+      const fetchWeather = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
+        .then((response) => response.json())
+        .then(data => {
+          console.log(data)
 
-    const fetchWeather = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
-  .then((response)=> response.json())
- .then(data => {console.log(data)
+          const curremtTemp = data.hourly.temperature_2m[0];
+          const arr = data.hourly.temperature_2m;
+          let sum = 0;
+          for (let number of arr) {
+            sum += number;
+          }
+          averageTemp = sum / arr.length;
 
-const curremtTemp = data.hourly.temperature_2m[0];
-const arr = data.hourly.temperature_2m; 
-let sum = 0;
-for (let number of arr) {
-    sum += number;
-}
-averageTemp = sum /arr.length;
+          average.innerHTML = "AVERAGE TEMPERATURE: " + parseInt(averageTemp)
+          temperature.innerHTML = curremtTemp
+          cityPlacement.innerHTML = cityName
+          country.innerHTML = countryName
+        })
+        .catch((error) => console.log(error));
 
-average.innerHTML="AVERAGE TEMPERATURE: " + parseInt(averageTemp)
-temperature.innerHTML = curremtTemp
-cityPlacement.innerHTML= cityName
-country.innerHTML = countryName
-})
- .catch((error)=> console.log(error));
- 
-})
-.catch((error)=> console.log(error))
+    })
+    .catch((error) => console.log(error))
 }
 
 
