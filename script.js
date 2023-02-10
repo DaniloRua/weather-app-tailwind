70e705263cfcdf36cefa0e901c5ed0f54f923d96
Danilopc <sr_danilo@hotmail.com>
2023-02-10 16:10:45 +0100

    average temp weekdays

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ const temperature = document.getElementById('temperature');
 const cityPlacement = document.getElementById('city');
 const country = document.getElementById('country');
 const average = document.getElementById('average');
-const city = document.getElementById('city-name')
+const city = document.getElementById('city-name');
+const nextDay1 = document.getElementById('next-day-1');
+const nextDay2 = document.getElementById('next-day-2');
+const nextDay3 = document.getElementById('next-day-3');
+const nextDay4 = document.getElementById('next-day-4');
+const nextDay5 = document.getElementById('next-day-5');
+
 let date = new Date();
-let currentHour = date.toTimeString().slice(0,2)
+let currentHour = date.toTimeString().slice(0,2);
+
+function sumArray (i){
+  sum = 0;
+  i.forEach( val => {
+   sum += val;
+});
+return sum;
+}
 
 city.addEventListener("keyup", function(e){
- if(e.code === 'Enter'|| "13"){
+ if(e.code === 'Enter'){
     setCity();
- }
-})
+ }})
+
+
 function setCity() {
 
   fetchCityName(city.value)
-
 }
 function fetchCityName(e) {
 
@@ -30,18 +44,36 @@ function fetchCityName(e) {
       const fetchWeather = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`)
         .then((response) => response.json())
         .then(data => {
-          console.log(data)
+          console.log(data)  
           const curremtTemp = data.hourly.temperature_2m[`${+currentHour}`];
-          const arr = data.hourly.temperature_2m;
-          let sum = 0;
-          for (let number of arr) {
-            sum += number;
-          }
-          averageTemp = sum / arr.length;
+          const temperatures = data.hourly.temperature_2m;
+
+          const temp24 = Object.values(temperatures).slice(0, 24);
+          const tempNextDay1 = Object.values(temperatures).slice(24, 48);
+          const tempNextDay2 = Object.values(temperatures).slice(48, 72);
+          const tempNextDay3 = Object.values(temperatures).slice(72, 96);
+          const tempNextDay4 = Object.values(temperatures).slice(96, 120);
+          const tempNextDay5 = Object.values(temperatures).slice(120, 144);
+
+          averageTemp = sumArray(temp24) / temp24.length;
+          averageNextDay1 = sumArray(tempNextDay1) / tempNextDay1.length;
+          averageNextDay2 = sumArray(tempNextDay2) / tempNextDay2.length;
+          averageNextDay3 = sumArray(tempNextDay3) / tempNextDay3.length;
+          averageNextDay4 = sumArray(tempNextDay4) / tempNextDay4.length;
+          averageNextDay5 = sumArray(tempNextDay5) / tempNextDay5.length;
+
+
+
           average.innerHTML = "AVERAGE: " + parseInt(averageTemp) + "°C";
           temperature.innerHTML = curremtTemp + "°C";
           cityPlacement.innerHTML = cityName;
           country.innerHTML = countryName;
+          nextDay1.innerHTML = parseInt(averageNextDay1) + "°C";
+          nextDay2.innerHTML = parseInt(averageNextDay2) + "°C";
+          nextDay3.innerHTML = parseInt(averageNextDay3) + "°C";
+          nextDay4.innerHTML = parseInt(averageNextDay4) + "°C";
+          nextDay5.innerHTML = parseInt(averageNextDay5) + "°C";
+
         })
         .catch((error) => console.log(error));
     })
